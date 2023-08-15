a8cef53482ad2f0ed4edec0abaf06d9752a773a9
Yodzhik <141939929+Yodzhik@users.noreply.github.com>
2023-08-15 17:27:57 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -2,30 +2,38 @@ import React from 'react';
 import axios from 'axios';
 import { useState } from 'react';
 import './App.css';
-import { Card, Container, Row, Col } from 'react-bootstrap';
+import { Card, Container, Row, Col, Button, Form } from 'react-bootstrap';
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { getWeatherAPI } from './service/api';
 
 
 function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=39b2f7865704c46a89117f4aaef142f1&units=metric`
+  const [change, setChange] = useState(false)
   const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const currentDate = new Date();
   const dayOfWeek = daysOfWeek[currentDate.getDay()];
 
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
-      axios.get(url).then((response) => {
-        setData(response.data)
-        console.log(response.data)
+      setChange(true)
+      setLocation('')
+      getWeatherAPI(event.target.value).then(res => {
+        if (res) {
+          setData(res)
+        } else {
+          setData('notFound')
+          setLocation('')
+        }
       })
     }
   }
 
   const weatherIcon = (condition) => {
     switch (condition) {
-      case 'Clear sky':
+      case 'Clear':
+        return <i className="bi bi-sun"></i>;
       case 'Sun':
         return <i className="bi bi-sun"></i>;
       case 'Clouds':
@@ -45,39 +53,47 @@ function App() {
 
   return (
     <div className="app">
+
       <Container>
-        <Row>
+        <Row className='justify-content-center p-5'>
           <Col xs={12} lg={6} xl={3}>
             <Card>
-              <Card.Body id='weather'>
-                <Row className='row row-cols-1'>
-                  <Col className='d-flex justify-content-center mb-5'>
-                    <h6>{data.name}</h6>
-                  </Col>
-                  <Col className='d-flex justify-content-center mb-5 mt-2'>
-                    {weatherIcon(data.weather ? data.weather[0].main : null)} 
-                  </Col>
-                  <Col className='d-flex justify-content-center mt-2'>
-                    {data.main ? <h1>{data.main.temp}°C</h1> : null}
-                  </Col>
-                  <Col className='d-flex justify-content-center mb-5 '>
-                    {data.main ? <h6>FeelLike {data.main.feels_like}</h6> : null}
-                  </Col>
-                  <Col className='d-flex justify-content-center mb-5'>
+              <Card.Body id='weather' className={'d-flex flex-column'}>
+                <div style={{minHeight: '400px'}}>
+                {data.name ?
+                <div>
+                  <h6>{data?.name}</h6>
+                  <div className='d-flex justify-content-center mb-5 mt-2'>
+                    {weatherIcon(data.weather ? data.weather[0].main : null)}
+                  </div>
+                  <div className='d-flex justify-content-center mt-2'>
+                    {data.main ? <h1>{Math.floor(data.main.temp)}°C</h1> : null}
+                  </div>
+                  <div className='d-flex justify-content-center mb-5 '>
+                    {data.main ? <h6>Ощущается как {Math.floor(data.main.feels_like)}°C</h6> : null}
+                  </div>
+                  <div className='d-flex justify-content-center mb-5'>
                     <h4>{dayOfWeek}</h4>
-                  </Col>
-                  <Col className='d-flex justify-content-center'>
-                    <input
+                  </div>
+                </div>
+                : data === 'notFound' &&
+                  <h4>Город не найден</h4>
+                }
+                </div>
+                <div className='d-flex justify-content-center'>
+                  {!change ?
+                    <Form.Control
                       value={location}
                       onChange={event => setLocation(event.target.value)}
                       onKeyUp={searchLocation}
                       type="text"
-                      className="form-control"
                       id="exampleFormControlInput1"
                       placeholder="Введите город"
                     />
-                  </Col>
-                </Row>
+                    :
+                    <Button variant={'outline-warning rounded-pill'} onClick={() => setChange(false)}>Изменить</Button>
+                  }
+                </div>
               </Card.Body>
             </Card>
           </Col>
